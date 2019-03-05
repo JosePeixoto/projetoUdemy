22bb5133b6b06ea0a371ba5a86538460beda36f0
Peixoto <alvespeixoto@yahoo.com.br>
2019-03-05 15:28:28 -0300

    Primeiro commit do projeto

---
+++ src/main/java/com/projeto/projetoudemy/ProjetoudemyApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.projetoudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoudemyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoudemyApplication.class, args);
+    }
+
+}
